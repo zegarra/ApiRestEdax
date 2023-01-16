2072920f56cdb5d476900ebf2c5966c14c4afd6f
zegarra <38090529+zegarra@users.noreply.github.com>
2023-01-16 14:23:04 -0500

    text
    comentario

--- src/app.js
+++ src/app.js
@@ -15,3 +15,13 @@ app.use((req, res, next) =>
 {res.status(404).json({message:'endpoint not fond'})})
 
 export default app;
+
+
+//crear carpeta 
+//npm init -y (crea el package.json)
+//npm i express (crear el node modules y package-lock.json)
+//crear index.js
+//node index.js
+//package.json >> "type": "module",
+//npm i nodemon -D
+//>>dev : nodemon index.js
